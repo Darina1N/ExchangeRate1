df63ee04edbc5ca5d47758daff8c3e9235ed5670
Darina Kolesarova <darina.kolesarova@kosickaakademia.sk>
2021-04-12 22:22:12 +0200

    GUI

---
+++ src/main/java/sk/kosickaakademia/kolesarova/exchange1/gui/Controller.java
@@ -0,0 +1,18 @@
+package sk.kosickaakademia.kolesarova.exchange1.gui;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    public Button btn_change;
+    public TextField txt_value;
+    public ComboBox cmbx_currency;
+    public TextField txt_result;
+    public TextField txt_rate;
+
+    public void change(ActionEvent actionEvent) {
+    }
+}
